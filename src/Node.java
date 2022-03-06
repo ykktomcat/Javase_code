d3bc9c81cffebe22e1112c492a6f82e6c27f7476
Ykktomcat <ykk1106@qq.com>
2022-03-06 19:57:32 +0800

    第三次提交

--- src/Node.java
+++ src/Node.java
@@ -1,34 +1,61 @@
 //封装一个超级链表 node
 public class Node {
+
     //一个是真正存入的数据，另一个是指向下一个的引用
     private Integer data;
     private Node next;
 
     //无参构造
     public Node() {
-
     }
-
     //有参构造
     public Node(Integer data, Node next) {
-
-    }
-
-    public Integer getData() {
-        return data;
+        this.data = data;
+        this.next = next;
     }
 
     public void setData(Integer data) {
         this.data = data;
     }
 
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
     public Node getNext() {
         return next;
     }
 
-    public void setNext(Node next) {
-        this.next = next;
-    }
+
+//    //无参构造
+//    public Node() {
+//
+//    }
+//
+//    //有参构造
+//    public Node(Integer data, Node next) {
+//
+//    }
+//
+//    public Integer getData() {
+//        return data;
+//    }
+//
+//    public void setData(Integer data) {
+//        this.data = data;
+//    }
+//
+//    public Node getNext() {
+//        return next;
+//    }
+//
+//    public void setNext(Node next) {
+//        this.next = next;
+//    }
 
 
 }
--- src/SuperLink.java
+++ src/SuperLink.java
@@ -2,16 +2,96 @@
 public class SuperLink {
     //链表的头部
     private Node head;
+
+    //操作标识，当前链表有多长
+    private int currentIndex = -1;
+
     // 对数据的增删改查
 
-    // 增加一个元素
-    private void add(int data) {
+    // 增加一个元素，头插法
+    public void add(int data) {
         if (head == null) {
             head = new Node(data, null);
+        } else {
+            //创建一个指向头部的node
+            Node node = new Node(data, head);
+            //新的node变成head
+            head = node;
+        }
+        currentIndex++;
+    }
+
+
+    // 指定位置增加一个元素，尾插法
+    public void add(int index,int data) {
+        if(head == null){
+            add(data);
+        }else{
+            //先拉住后面的节点，再让前面的拉上自己。
+            //找到下标为index -1的节点
+            Node node = head;
+            for (int i = 0; i < index -1; i++) {
+                node = node.getNext();
+            }
+            //插入的核心
+            Node temp = new Node(data,node.getNext());
+            node.setNext(temp);
+            currentIndex++;
         }
 
     }
 
+
+    //修改一个元素
+    public void set(int index, int data) {
+        //找到要修改的元素位置，自己存储的值改成data
+
+        //找到下标为index的节点
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.getNext();
+        }
+        //修改的核心
+        node.setData(data);
+    }
+
+    //删除一个元素
+    public void delete(int index) {
+        if (index == 0) {
+            head = head.getNext();
+        } else {
+            //定义一个空的放在头部
+            Node node = head;
+            //找到下标为index - 1的节点
+            for (int i = 0; i < index - 1; i++) {
+                node = node.getNext();
+            }
+            //删除的核心
+            node.setNext(node.getNext().getNext());
+        }
+        currentIndex--;
+
+    }
+
+    //根据下标查找一个元素
+    public Integer select(int index) {
+        //定义一个空的放在头部
+        Node node = head;
+        //找到下标为index 的节点
+        for (int i = 0; i < index; i++) {
+            node = node.getNext();
+        }
+        return node.getData();
+    }
+
+    //链表转成字符串数组
+    public String linkToString() {
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < currentIndex + 1; i++) {
+            result.append(select(i)).append(",");
+        }
+        return result.substring(0, result.length() - 1) + "]";
+    }
 
 }
 
