2fec21bf3d301bb5cdefc32c0330fadeed3ed07c
Ykktomcat <ykk1106@qq.com>
2022-03-06 17:36:44 +0800

    第二次提交

---
+++ src/SuperLink.java
@@ -0,0 +1,17 @@
+//超级链表
+public class SuperLink {
+    //链表的头部
+    private Node head;
+    // 对数据的增删改查
+
+    // 增加一个元素
+    private void add(int data) {
+        if (head == null) {
+            head = new Node(data, null);
+        }
+
+    }
+
+
+}
+
